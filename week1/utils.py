f9189cc30c5cca28a2e924dc116b1ee3fe10f6a0
dimple <tina952122@gmail.com>
2026-07-23 01:37:29 +0530

    Update utils.py

--- week1/utils.py
+++ week1/utils.py
@@ -1,13 +1,9 @@
-
 from constants import *
+
 
 def clear_screen():
     print("\n" * 40)
 
-
-def print_grid(grid):
-    for row in grid:
-        print(" ".join(row))
 
 def display_grid(grid):
     for row in grid:
@@ -15,18 +11,20 @@ def display_grid(grid):
             print(cell, end=" ")
         print()
 
+
 def create_grid():
     grid = []
     for i in range(ROWS):
         row = [EMPTY] * COLS
         grid.append(row)
-    
-    grid[0][9] = OBSTACLE
+
+    grid[0][COLS - 1] = OBSTACLE
     grid[1][0] = OBSTACLE
-    grid[1][9] = OBSTACLE
+    grid[1][COLS - 1] = OBSTACLE
     grid[2][0] = OBSTACLE
-    grid[2][9] = OBSTACLE
+    grid[2][COLS - 1] = OBSTACLE
     return grid
+
 
 def move_player(grid, player_row, player_col, direction):
     new_row = player_row
@@ -34,14 +32,14 @@ def move_player(grid, player_row, player_col, direction):
 
     if direction == "W" or direction == "w":
         new_row = player_row - 1
-        
+
     elif direction == "S" or direction == "s":
         new_row = player_row + 1
 
     elif direction == "A" or direction == "a":
         new_col = player_col - 1
 
-    elif direction == "D" or direction == "d" :
+    elif direction == "D" or direction == "d":
         new_col = player_col + 1
 
     if 0 <= new_row < ROWS and 0 <= new_col < COLS:
@@ -53,5 +51,5 @@ def move_player(grid, player_row, player_col, direction):
             print("Invalid Move")
     else:
         print("Invalid Move")
-    
-    return player_row, player_col        
+
+    return player_row, player_col
