0280dcae1adca20bc7ff99390ef244ee0ba8d475
dimple <tina952122@gmail.com>
2026-07-23 00:03:24 +0530

    Update player.py

--- week 3/player.py
+++ week 3/player.py
@@ -1,14 +1,10 @@
 class Player:
     def __init__(self, row, col, color, number):
-        self.pos = (row, col)
         self.color = color
-        self.dir = (1, 0)
-        self.trail = []
-        self.alive = True
         self.number = number
-        self.on_territory = True
         self.start_pos = (row, col)
         self.start_dir = (1, 0)
+        self.reset()
 
     def reset(self):
         self.pos = self.start_pos
